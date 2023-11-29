5d871c174b75810e9245f715ca18db3770278d60
Oceane Villeneuve <oceanevilleneuve16@gmail.com>
2023-11-29 19:26:13 +0100

    add theme support

--- app/public/wp-content/themes/alexastudiocreation/inc/classes/class-alexastudiocreation-theme.php
+++ app/public/wp-content/themes/alexastudiocreation/inc/classes/class-alexastudiocreation-theme.php
@@ -34,5 +34,33 @@ class ALEXASTUDIOCREATION_THEME {
   public function setup_theme() {
     add_theme_support( 'title-tag' );
 
+    add_theme_support( 'post-thumbnails' );
+
+    add_theme_support( 'customize-selective-refresh-widgets' );
+
+    add_theme_support( 'automatic-feed-links' );
+
+    add_theme_support(
+      'html5',
+      [
+        'search-form',
+        'comment-form',
+        'comment-list',
+        'gallery',
+        'caption',
+        'script',
+        'style',
+      ]
+    );
+
+    add_editor_style();
+    add_theme_support( 'wp-block-styles' );
+
+    add_theme_support( 'align-wide' );
+
+    global $content_width;
+    if ( ! isset( $content_width ) ) {
+      $content_width = 1240;
+    }
   }
 }
