58fbd767c900c955c25f303788e779f6f8ed3e21
Oceane Villeneuve <oceanevilleneuve16@gmail.com>
2023-12-12 15:57:48 +0100

    made the navbar

--- app/public/wp-content/themes/alexastudiocreation/header.php
+++ app/public/wp-content/themes/alexastudiocreation/header.php
@@ -13,6 +13,9 @@
   <meta name="viewport" content="width=device-width, initial-scale=1" />
   <meta http-equiv="X-UA-Compatible" content="ie=edge">
   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css">
+  <link rel="preconnect" href="https://fonts.googleapis.com">
+  <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+  <link href="https://fonts.googleapis.com/css2?family=Italianno&display=swap" rel="stylesheet">
   <?php wp_head(); ?>
 
 </head>
@@ -25,7 +28,16 @@ if ( function_exists( 'wp_body_open' ) ) {
 ?>
 
 <div id="page" class="site">
-  <header id="masthead" class="site-header" role="banner">
-    <?php get_template_part( 'template-parts/header/nav' ); ?>
-  </header>
+  <div class="d-flex justify-content-sm-between">
+    <header id="masthead" class="site-header" role="banner">
+      <?php get_template_part( 'template-parts/header/nav' ); ?>
+    </header>
+    <div class="mt-4">
+
+      <!-- LINK RENVOI A LA HOME -->
+      <a href="<?php echo esc_url( home_url( '/' ) ); ?>" class="link-to-home">Alexa Studio Création</a>
+      <p class="sub-title-header">Photographe - Vidéaste </p>
+    </div>
+    <span></span>
+  </div>
   <div id="content" class="site-content">
