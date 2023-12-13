b900e9ca4b6c206a10da018f9ee3ed6c80a7c9d6
Oceane Villeneuve <oceanevilleneuve16@gmail.com>
2023-12-13 14:31:55 +0100

    render pages title and their content

--- app/public/wp-content/themes/alexastudiocreation/page.php
+++ app/public/wp-content/themes/alexastudiocreation/page.php
@@ -9,6 +9,36 @@ get_header();
 
 ?>
 
-<div>Single Page</div>
+
+<div id="secondary" class="post-page-container">
+  <main id="main" class="site-main mt-5" role="main">
+      <div class="container">
+
+          <?php
+          if (have_posts()) :
+              while (have_posts()) : the_post();
+          ?>
+                  <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+                      <header class="entry-header">
+                          <h1 class="single-post-custom-title"><?php the_title(); ?></h1>
+                      </header>
+
+                      <div class="entry-content">
+                          <?php the_content(); ?>
+                      </div>
+
+                  </article>
+
+          <?php
+              endwhile;
+          else :
+              get_template_part('template-parts/content-none');
+          endif;
+          ?>
+      </div>
+        <!-- BOUTON CONTACT -->
+      <a href="<?php echo esc_url(get_permalink(get_page_by_title('Contact'))); ?>" class="contact-button">Contact</a>
+  </main>
+</div>
 
 <?php get_footer(); ?>
